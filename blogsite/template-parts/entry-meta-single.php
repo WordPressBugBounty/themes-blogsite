79205c95d826ca82e047a1281144c0f65417d36f
Sean Murphy <sean@iamseanmurphy.com>
2024-10-14 01:05:18 +0000

    1.1.1

---
+++ blogsite/template-parts/entry-meta-single.php
@@ -0,0 +1,10 @@
+<div class="entry-meta">
+
+	<span class="entry-category"><?php blogsite_first_category(); ?></span>
+	<span class="entry-author"><?php esc_url( the_author_posts_link() ); ?></span>
+	<span class="sep author-sep">&middot;</span>	
+	<span class="entry-date"><?php echo get_the_date(); ?></span>
+	<span class="sep">&middot;</span>
+	<span class='entry-comment'><?php comments_popup_link( __('0 Comment','blogsite'), __('1 Comment','blogsite'), __('% Comments','blogsite'), 'comments-link', __('Comments off','blogsite')); ?></span>
+	
+</div>
